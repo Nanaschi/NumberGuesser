c6c98c2c9d30b95cd9b1a4b38e1712bb36b9ba6f
Nanaschi <naturforscher@outlook.com>
2021-01-13 01:31:25 +0700

    0.1

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+    {
+    [SerializeField] int minValue;
+    [SerializeField] int maxValue;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+    void StartGame()
+    {
+
+        NextGuess();
+   
+    }
+
+    // Update is called once per frame
+
+    void NextGuess()
+    {
+        guess = Random.Range(minValue, maxValue);
+        
+        guessText.text = guess.ToString();
+    }
+    public void OnPressHigher()
+    {
+        minValue = guess + 1;
+        NextGuess();
+     
+    }
+    public void OnPressLower()
+    {
+        maxValue = guess - 1;
+        NextGuess();
+
+    }
+}
